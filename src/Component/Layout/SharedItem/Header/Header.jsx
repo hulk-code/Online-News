286b2371340ca88392061ab096042198489bb568
Akash <shoaibsarkarakash019@gmail.com>
2024-12-02 22:17:09 +0600

    added Header with realtime date

--- src/Component/Layout/SharedItem/Header/Header.jsx
+++ src/Component/Layout/SharedItem/Header/Header.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
-
+import moment from 'moment';
+import logo from '../../../../assets/logo.png'
 const Header = () => {
     return (
-        <div>
-            this is header
+        <div className='w-1/2 mx-auto mb-5 mt-5'>
+         <img src={logo} alt="" />
+         <h1 className=' md:ml-20 font-poppins font-thin'>Journalism Without Fear or Favour</h1>
+         <p className=' md:ml-20 font-poppins font-bold'>{moment().format("dddd, MMMM Do YYYY, h:mm:ss a")}</p>
         </div>
     );
 };
