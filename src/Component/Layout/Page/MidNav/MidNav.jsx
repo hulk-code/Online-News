c395b9c083bf5683e924fc48e5772619628a6c5d
unknown <shoaibsarkarakash019@gmail.com>
2024-12-07 11:16:57 +0600

    create dynamic next and pervious news button using paggination

--- src/Component/Layout/Page/MidNav/MidNav.jsx
+++ src/Component/Layout/Page/MidNav/MidNav.jsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import { useEffect, useState } from "react";
+import AllNews from "./AllNews/AllNews";
 
 const MidNav = () => {
-    return (
-        <div>
-            <h1>Mid Nav</h1>
-        </div>
-    );
+  const [allNews, setAllNews] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemsPerPage = 3;
+
+  useEffect(() => {
+    fetch("../../../../../public/news.json")
+      .then((res) => res.json())
+      .then((data) => setAllNews(data));
+  }, []);
+
+  const startIndex = currentPage * itemsPerPage;
+  const currentNews = allNews.slice(startIndex, startIndex + itemsPerPage);
+
+  const handleNext = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  const handlePrevious = () => {
+    setCurrentPage((prevPage) => prevPage - 1);
+  };
+
+  return (
+    <div>
+      <h1 className="font-poppins text-lg font-bold mb-5">Dargon News Home</h1>
+      <div className="space-y-4  flex flex-col items-center">
+        {currentNews.map((todayNews) => (
+          <AllNews key={todayNews.id} todayNews={todayNews}></AllNews>
+        ))}
+      </div>
+      <div className="flex justify-between mt-4">
+        {currentPage > 0 && (
+          <button
+            onClick={handlePrevious}
+            className="bg-blue-500 text-white px-4 py-2 rounded transition duration-300 ease-in-out transform hover:bg-blue-700 hover:scale-105 hover:shadow-lg"
+          >
+            Previous
+          </button>
+        )}
+        {startIndex + itemsPerPage < allNews.length && (
+          <button
+            onClick={handleNext}
+            className="bg-green-500 text-white px-4 py-2 rounded transition duration-300 ease-in-out transform hover:bg-green-700 hover:scale-105 hover:shadow-lg"
+          >
+            Next
+          </button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default MidNav;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "127.0.0.1", // Use IPv4 instead of the default ::1 (IPv6)
+    port: 3000, // Change the port to avoid conflicts (default is 5173)
+  },
+});
